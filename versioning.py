312838f098063af16508aac8613e5897a9115941
harui2019 <harui2019@proton.me>
2024-11-08 19:49:12 +0800

    fix(versioning): wrong tag list cmd

--- versioning.py
+++ versioning.py
@@ -11,17 +11,20 @@ from qurry import __version__
 if __name__ == "__main__":
     CURRENT_DATE_STRING = datetime.datetime.now().strftime("%Y%m%d")
     STABLE_VERSION = ".".join(__version__.split(".")[:3])
+    TAG_CHECK = STABLE_VERSION + "*"
 
-    LABELS = subprocess.check_output(["git", "tag", "-l", STABLE_VERSION]).strip()
+    print(f"| Current Qurry version: {__version__}")
+    print(f"| Current date string: {CURRENT_DATE_STRING}")
+    print(f"| Tagging check by: {TAG_CHECK}")
+    LABELS = subprocess.check_output(["git", "tag", "-l", TAG_CHECK]).strip()
     LABELS = LABELS.decode("utf-8")
     LABEL_NUM = 0 if LABELS == "" else len(LABELS.split("\n"))
 
-    VERSION = STABLE_VERSION + f"rc{LABEL_NUM+1}"
+    VERSION = STABLE_VERSION + f"rc{LABEL_NUM + 1}"
 
-    print(f"| Current Qurry version: {__version__}")
     print(f"| CURRENT_DATE_STRING: {CURRENT_DATE_STRING}, label_num: {LABEL_NUM}")
     print(f"| Version: {VERSION}")
-    print(f"| Release candidate: {LABEL_NUM+1}")
+    print(f"| Release candidate: {LABEL_NUM + 1}")
     subprocess.run(
         ["git", "tag", "-a", VERSION, "-m", f"chore: Release candidate {VERSION}"],
         check=True,
